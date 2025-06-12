4aae5f301d6a99073e6f9149aea2e0de3d7b1cd4
Akshay Sonawane <akshaysonawane4337@gmail.com>
2025-06-12 14:06:46 +0530

    Required classes

---
+++ src/main/java/com/sunbeam/VastralApplication.java
@@ -0,0 +1,13 @@
+package com.sunbeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VastralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VastralApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sunbeam/response/ApiResponse.java
@@ -0,0 +1,33 @@
+package com.sunbeam.response;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class ApiResponse {
+    private String message;
+
+    public ApiResponse() {}
+
+    public ApiResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
